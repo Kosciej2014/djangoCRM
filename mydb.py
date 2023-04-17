1e610b8432c6d003185a36c4fb7c2941147d10a8
Kamil <58975217+Kosciej2014@users.noreply.github.com>
2023-04-17 21:36:08 +0200

    Update mydb.py
    Delete login db

--- mydb.py
+++ mydb.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 dataBase = mysql.connector.connect(
-    host= 'localhost',
-    user = 'root',
-    passwd = 'Djangocrm1234'
+    host= '',
+    user = '',
+    passwd = ''
 
 )
 
